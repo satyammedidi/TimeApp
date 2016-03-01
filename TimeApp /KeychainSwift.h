f46477f83bdf8c2a4291dfdd993d5b10dc7c1518
medidi vv satyanarayana murty <medidi.murty@exilant.com>
2016-03-01 11:21:10 +0530

    satyam added the KeychainSwift files

---
+++ TimeApp /KeychainSwift.h
@@ -0,0 +1,19 @@
+//
+//  KeychainSwift.h
+//  TimeApp
+//
+//  Created by medidi vv satyanarayana murty on 01/03/16.
+//  Copyright © 2016 medidi vv satyanarayana murty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeychainSwift.
+FOUNDATION_EXPORT double KeychainSwiftVersionNumber;
+
+//! Project version string for KeychainSwift.
+FOUNDATION_EXPORT const unsigned char KeychainSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeychainSwift/PublicHeader.h>
+
+
